3f1904e7c839a3f476c7b64fd725fd24918ee182
Alpha Chen and Andres Medina <pair+achen+amedina@pivotal.io>
2016-02-02 15:48:56 -0800

    Added class to retrieve/save/delete favorite stops

---
+++ app/src/main/java/io/pivotal/weatherbus/app/SavedStops.java
@@ -0,0 +1,60 @@
+package io.pivotal.weatherbus.app;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SavedStops {
+    SharedPreferences settings;
+
+    public SavedStops(SharedPreferences settings) {
+        this.settings = settings;
+    }
+
+    public List<String> getSavedStops() {
+        List<String> result = new ArrayList<String>();
+
+        String savedStops = settings.getString("saved_stops", "");
+        if (savedStops.equals("")) {
+            return result;
+        }
+
+        for (String stop : savedStops.split(",")) {
+            result.add(stop);
+        }
+        return result;
+    }
+
+    public void addSavedStop(String stop) {
+        String result = "";
+        List<String> savedStops = getSavedStops();
+
+        for (String savedStop : savedStops) {
+            result += savedStop + ",";
+        }
+        result += stop;
+
+        Editor editor = settings.edit();
+        editor.putString("saved_stops", result);
+        editor.commit();
+    }
+
+    public void deleteSavedStop(String stop) {
+        String result = "";
+        List<String> savedStops = getSavedStops();
+
+        for (String savedStop : savedStops) {
+            if (savedStop.equals(stop)) {
+                continue;
+            }
+            result += savedStop + ",";
+        }
+        result = result.substring(0,result.length()-1);
+
+        Editor editor = settings.edit();
+        editor.putString("saved_stops", result);
+        editor.commit();
+    }
+}
---
+++ app/src/test/java/io/pivotal/weatherbus/app/SavedStopsTest.java
@@ -0,0 +1,86 @@
+package io.pivotal.weatherbus.app;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import io.pivotal.weatherbus.app.testUtils.WeatherBusTestRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(WeatherBusTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class SavedStopsTest {
+    @Mock
+    SharedPreferences settings;
+    @Mock
+    Editor editor;
+
+    SavedStops subject;
+
+    @Before
+    public void setUp() {
+        subject = new SavedStops(settings);
+        when(settings.edit()).thenReturn(editor);
+    }
+
+    @Test
+    public void onNoSavedStops_shouldHaveEmptyList() {
+        when(settings.getString("saved_stops","")).thenReturn("");
+
+        assertThat(subject.getSavedStops().size()).isEqualTo(0);
+    }
+
+    @Test
+    public void onSomeSavedStops_shouldHaveSavedStops() {
+        when(settings.getString("saved_stops","")).thenReturn("1_123,1_456,1_789");
+
+        assertThat(subject.getSavedStops()).isEqualTo(new ArrayList<String>() {{
+            add("1_123");
+            add("1_456");
+            add("1_789");
+        }});
+    }
+
+    @Test
+    public void onAddingNewSavedStop_shouldSaveNewStops() {
+        when(settings.getString("saved_stops","")).thenReturn("");
+
+        subject.addSavedStop("1_589");
+
+        verify(editor,times(1)).putString("saved_stops","1_589");
+        verify(editor,times(1)).commit();
+    }
+
+    @Test
+    public void onAddingSavedStops_shouldSaveAllStops() {
+        when(settings.getString("saved_stops","")).thenReturn("1_123,1_456,1_789");
+
+        subject.addSavedStop("1_589");
+
+        verify(editor,times(1)).putString("saved_stops","1_123,1_456,1_789,1_589");
+        verify(editor,times(1)).commit();
+    }
+
+    @Test
+    public void whenDeletingAnExistingStop_itShouldDeleteFromSettings() {
+        when(settings.getString("saved_stops","")).thenReturn("1_123,1_456,1_789");
+        subject.deleteSavedStop("1_456");
+        verify(editor,times(1)).putString("saved_stops","1_123,1_789");
+        verify(editor,times(1)).commit();
+    }
+
+    @Test
+    public void whenDeletingANotSavedStop_itShouldDoNothing() {
+        when(settings.getString("saved_stops","")).thenReturn("1_123,1_456,1_789");
+        subject.deleteSavedStop("1_589");
+        verify(editor,times(1)).putString("saved_stops","1_123,1_456,1_789");
+        verify(editor,times(1)).commit();
+    }
+}
